669dab17a8b99fee42d10700b6c7581b33ffb2a4
Oliver <oliver.henry.walters@gmail.com>
2016-03-20 16:54:06 +1100

    Improvements to WE-MAPI script

--- scripts/Inductors/we-mapi.py
+++ scripts/Inductors/we-mapi.py
@@ -48,46 +48,49 @@ desc = "Inductor, Würth Elektronik, {pn}"
 tags = "inductor wurth smd"
 
 for inductor in inductors:
-	name,l,w,x,g,y = inductor
-	
-	fp_name = prefix + part.format(pn=str(name))
-	
-	fp = Footprint(fp_name)
-	
-	description = desc.format(pn = part.format(pn=str(name))) + ", " + dims.format(l=l,w=w)
-	
-	fp.setTags(tags)
+    name,l,w,x,g,y = inductor
+    
+    fp_name = prefix + part.format(pn=str(name))
+    
+    fp = Footprint(fp_name)
+    
+    description = desc.format(pn = part.format(pn=str(name))) + ", " + dims.format(l=l,w=w)
+    
+    fp.setTags(tags)
     fp.setAttribute("smd")
-	fp.setDescription(description)
-	
-	# set general values
-	fp.append(Text(type='reference', text='REF**', at=[0,-y/2 - 1], layer='F.SilkS'))
-	fp.append(Text(type='value', text=fp_name, at=[0,y/2 + 1.5], layer='F.Fab'))
-	
-	#calculate pad center
-	#pad-width pw
-	pw = (x-g) / 2
-	c = g/2 + pw/2
-	
-	layers = ["F.Cu","F.Paste","F.Mask"]
-	
-	#add pads
-	fp.append(Pad(number=1,at=[-c,0],layers=layers,shape=Pad.SHAPE_RECT,type=Pad.TYPE_SMT,size=[pw,y]))
-	fp.append(Pad(number=2,at=[c,0],layers=layers,shape=Pad.SHAPE_RECT,type=Pad.TYPE_SMT,size=[pw,y]))
-	
-	#add inductor outline
-	fp.append(RectLine(start=[-l/2,-w/2],end=[l/2,w/2],width=0.05,layer="Eco1.User"))
-	
-	#add lines
-	fp.append(Line(start=[-g/2+0.2,-w/2-0.1],end=[g/2-0.2,-w/2-0.1]))
-	fp.append(Line(start=[-g/2+0.2,w/2+0.1],end=[g/2-0.2,w/2+0.1]))
-	
-	#Add a model
-	fp.append(Model(filename="Inductors.3dshapes/" + fp_name + ".wrl"))
-	
-	#filename
-	filename = output_dir + fp_name + ".kicad_mod"
-	
-	file_handler = KicadFileHandler(fp)
-	file_handler.writeFile(filename)
-	
+    fp.setDescription(description)
+    
+    # set general values
+    fp.append(Text(type='reference', text='REF**', at=[0,-y/2 - 1], layer='F.SilkS'))
+    fp.append(Text(type='value', text=fp_name, at=[0,y/2 + 1.5], layer='F.Fab'))
+    
+    #calculate pad center
+    #pad-width pw
+    pw = (x-g) / 2
+    c = g/2 + pw/2
+    
+    layers = ["F.Cu","F.Paste","F.Mask"]
+    
+    #add pads
+    fp.append(Pad(number=1,at=[-c,0],layers=layers,shape=Pad.SHAPE_RECT,type=Pad.TYPE_SMT,size=[pw,y]))
+    fp.append(Pad(number=2,at=[c,0],layers=layers,shape=Pad.SHAPE_RECT,type=Pad.TYPE_SMT,size=[pw,y]))
+    
+    #add inductor courtyard
+    cx = c + pw/2
+    cy = y / 2
+    
+    fp.append(RectLine(start=[-cx,-cy],end=[cx,cy],offset=0.35,width=0.05,grid=0.05,layer="F.CrtYd"))
+    
+    #add lines
+    fp.append(Line(start=[-g/2+0.2,-w/2-0.1],end=[g/2-0.2,-w/2-0.1]))
+    fp.append(Line(start=[-g/2+0.2,w/2+0.1],end=[g/2-0.2,w/2+0.1]))
+    
+    #Add a model
+    fp.append(Model(filename="Inductors.3dshapes/" + fp_name + ".wrl"))
+    
+    #filename
+    filename = output_dir + fp_name + ".kicad_mod"
+    
+    file_handler = KicadFileHandler(fp)
+    file_handler.writeFile(filename)
+    
