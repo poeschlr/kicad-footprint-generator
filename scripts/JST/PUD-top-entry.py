47c192bc7ff8d4b9c32e2b975931b126754569ca
Oliver <oliver.henry.walters@gmail.com>
2016-03-22 22:36:19 +1100

    Added PUD scripts Added J2100-side-entry script

--- scripts/JST/PUD-top-entry.py
+++ scripts/JST/PUD-top-entry.py
@@ -44,16 +44,16 @@ footprint specific details to go here
 Datasheet: http://www.jst-mfg.com/product/pdf/eng/eXH.pdf
 
 """
-pitch = 2.50
+pitch = 2.0
 
-pincount = [3,4,5,6,8,10] #number of pins in each row
+pincount = [4,5,6,8,10,11,12,13,14,15,16,17,18,19,20] #number of pins in each row
 rows = 2
-row_pitch = 4
+row_pitch = 2
 
 #FP name strings
-part = "B{n:02}B-J21DK-GG" #JST part number format string
+part = "B{n:02}B-PUDSS" #JST part number format string
 
-prefix = "JST_J2100_"
+prefix = "JST_PUD_"
 suffix = "_2x{n:02}x{p:.2f}mm_Straight"
 
 #FP description and tags
@@ -64,39 +64,34 @@ if __name__ == '__main__':
         
         #calculate fp dimensions
         A = (pins - 1) * pitch
-        B = A + 5.2
+        B = A + 4
     
         #generate the name
         fp_name = prefix + part.format(n=2*pins) + suffix.format(n=pins, p=pitch)
 
         footprint = Footprint(fp_name)
         
-        description = "JST J2100 series connector, dual row, center locking, " + part.format(n=2*pins) + ", top entry type, through hole"
+        description = "JST PUD series connector, dual row, " + part.format(n=2*pins) + ", top entry type, through hole"
         
         #set the FP description
         footprint.setDescription(description)
         
-        tags = "connector jst j2100 vertical"
+        tags = "connector jst pud vertical"
         
         #set the FP tags
         footprint.setTags(tags)
 
         # set general values
-        footprint.append(Text(type='reference', text='REF**', at=[A/2,-9.7], layer='F.SilkS'))
-        footprint.append(Text(type='value', text=fp_name, at=[A/2,7.2], layer='F.Fab'))
+        footprint.append(Text(type='reference', text='REF**', at=[A/2,-5.2], layer='F.SilkS'))
+        footprint.append(Text(type='value', text=fp_name, at=[A/2,5.8], layer='F.Fab'))
             
-        #generate the pads (row 1)
-        pa1 = PadArray(pincount=pins, x_spacing=pitch, type=Pad.TYPE_THT, shape=Pad.SHAPE_CIRCLE, increment=2, size=1.6, drill=0.9, layers=['*.Cu','*.Mask'])
-        pa2 = PadArray(pincount=pins, x_spacing=pitch, type=Pad.TYPE_THT, shape=Pad.SHAPE_CIRCLE, start=[0,-row_pitch], initial=2, increment=2, size=1.6, drill=0.9, layers=['*.Cu','*.Mask'])
-        
-        footprint.append(pa1)
-        footprint.append(pa2)
+
         
         #draw the component outline
         x1 = A/2 - B/2
         x2 = x1 + B
-        y1 = -4-4.48
-        y2 = y1 + 14.4
+        y2 = 2 + 2.4
+        y1 = y2 - 8.3
         
         #offset off
         off = 0.1
@@ -106,23 +101,36 @@ if __name__ == '__main__':
         x2 += off
         y2 += off
         
+        #draw outline
+        footprint.append(RectLine(start=[x1,y1],end=[x2,y2]))
+        
+        #wall thickness t
+        t = 0.75
+        
+        #draw inside tab
+        T = A/2 + 0.5
+        footprint.append(RectLine(start=[A/2-T/2,y1+t],end=[A/2+T/2,y1+2*t]))
+        
         #outline
-        ol = RectLine(start=[x1,y1],end=[x2,y2])
-        footprint.append(ol)
+        side = [
+        {'x': A/2-T/2-t,'y': y1},
+        {'x': A/2-T/2-t,'y': y1 + 2 * t},
+        {'x': A/2-T/2-2*t,'y': y1 + 2 * t},
+        {'x': A/2-T/2-2*t,'y': y1 + t},
+        {'x': x1 + t,'y': y1 + t},
+        {'x': x1 + t,'y': y2 - t},
+        {'x': A/2,'y': y2 - t},
+        ]
+        
+        footprint.append(PolygoneLine(polygone=side))
+        footprint.append(PolygoneLine(polygone=side,x_mirror=A/2))
         
         #courtyard
         cy = RectLine(start=[x1,y1],end=[x2,y2],offset=0.5,layer='F.CrtYd',width=0.05,grid=0.05)
         footprint.append(cy)
         
-        #add mounting holes 
-        m1 = Pad(at=[0,3.3],layers=["*.Cu",'*.Mask'],shape=Pad.SHAPE_CIRCLE,type=Pad.TYPE_THT,size=3, drill=2)
-        m2 = Pad(at=[A,3.3],layers=["*.Cu",'*.Mask'],shape=Pad.SHAPE_CIRCLE,type=Pad.TYPE_THT,size=3, drill=2)
-        
-        footprint.append(m1)
-        footprint.append(m2)
-        
         #add p1 marker
-        px = -3
+        px = -2.55
         m = 0.3
         
         marker = [
@@ -134,32 +142,15 @@ if __name__ == '__main__':
         
         footprint.append(PolygoneLine(polygone=marker))
         
-        #line offset o
-        o = 1
-        ya = o
-        yb = -row_pitch - o
-        #draw lines between pin pairs
-        for i in range(pins-1):
-            x = (i + 0.5) * pitch
-            footprint.append(Line(start=[x,ya],end=[x,yb]))
-            
-        #draw the inside of the connector
-        #connector thickness t
-        t = 0.55
-        #notch size n
-        n = 1.2
-        inside = [
-        {'x': A/2 - n/2,'y': y1},
-        {'x': A/2 - n/2,'y': y1 + t},
-        {'x': x1 + t,'y': y1 + t},
-        {'x': x1 + t,'y': y2 - t},
-        {'x': x1 + t + n,'y': y2 - t},
-        {'x': x1 + t + n,'y': y2 - 2 * t},
-        {'x': A/2,'y': y2 - 2 * t},
-        ]
+        #generate the pads (row 1)
         
-        footprint.append(PolygoneLine(polygone = inside))
-        footprint.append(PolygoneLine(polygone = inside, x_mirror=A/2))
+        drill = 0.7
+        size = 1.3
+        pa1 = PadArray(pincount=pins, x_spacing=pitch, type=Pad.TYPE_THT, shape=Pad.SHAPE_CIRCLE, increment=2, size=size, drill=drill, layers=['*.Cu','*.Mask'])
+        pa2 = PadArray(pincount=pins, x_spacing=pitch, type=Pad.TYPE_THT, shape=Pad.SHAPE_CIRCLE, start=[0,row_pitch], initial=2, increment=2, size=size, drill=drill, layers=['*.Cu','*.Mask'])
+        
+        footprint.append(pa1)
+        footprint.append(pa2)
         
         #Add a model
         footprint.append(Model(filename="Connectors_JST.3dshapes/" + fp_name + ".wrl"))
