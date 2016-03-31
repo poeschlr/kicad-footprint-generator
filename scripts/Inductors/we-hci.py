a50e6c982d3a501f5371b43f23b2bbf3646e61fb
Oliver Walters <oliver@currawongeng.com>
2016-04-01 09:43:27 +1100

    Fixes to wurth inductor scripts

--- scripts/Inductors/we-hci.py
+++ scripts/Inductors/we-hci.py
@@ -3,17 +3,19 @@
 #sizes,shapes,etc]
 #name, L, W, pad-w, pad-gap, pad-h
 inductors = [
-[1610,1.6,1.6,1.8,0.4,1.8],
-[2010,2.0,1.6,2.3,0.6,1.9],
-[2506,2.5,2.0,2.8,1.1,2.3],
-[2508,2.5,2.0,2.8,1.1,2.3],
-[2510,2.5,2.0,2.8,1.1,2.3],
-[2512,2.5,2.0,2.8,1.1,2.3],
-[3010,3.0,3.0,3.4,0.8,3.4],
-[3012,3.0,3.0,3.4,0.8,3.4],
-[3015,3.0,3.0,3.4,0.8,3.4],
-[3020,3.0,3.0,3.5,0.8,3.4],
-[4020,4.0,4.0,3.35,1.39,3.7],
+[5040,5.5,5.2,1.9,2.2,1.6],
+[7030,6.9,6.9,2.7,2.4,2.2],
+[7040,6.9,6.9,2.7,2.4,2.2],
+[7050,6.9,6.9,2.7,2.4,2.2],
+[1030,10.6,10.6,3.8,3.9,3.5],
+[1040,10.2,10.2,3.5,4.5,4],
+[1050,10.2,10.2,3.85,3.8,4],
+[1335,12.8,12.8,3.85,6.3,5],
+[1350,12.8,12.8,4.5,6,5],
+[1365,12.8,12.8,4.5,6,5],
+[2212,22.5,22.0,7,9.5,7],
+[1890,18.2,18.2,6,7.3,6],
+[1890,18.2,18.2,6,7.3,6],
 ]
 
 import sys
@@ -41,11 +43,11 @@ from KicadModTree import *
 from KicadModTree.nodes.specialized.PadArray import PadArray
 
 prefix = "Inductor_"
-part = "WE-MAPI-{pn}"
+part = "WE-HCI-{pn}"
 dims = "{l:0.1f}mmx{w:0.1f}mm"
 
 desc = "Inductor, Wurth Elektronik, {pn}"
-tags = "inductor wurth smd"
+tags = "inductor wurth hci smd"
 
 for inductor in inductors:
     name,l,w,x,g,y = inductor
@@ -61,12 +63,13 @@ for inductor in inductors:
     fp.setDescription(description)
     
     # set general values
-    fp.append(Text(type='reference', text='REF**', at=[0,-y/2 - 1], layer='F.SilkS'))
-    fp.append(Text(type='value', text=fp_name, at=[0,y/2 + 1.5], layer='F.Fab'))
+    fp.append(Text(type='reference', text='REF**', at=[0,-w/2 - 1], layer='F.SilkS'))
+    fp.append(Text(type='value', text=fp_name, at=[0,w/2 + 1.5], layer='F.Fab'))
+
     
     #calculate pad center
     #pad-width pw
-    pw = (x-g) / 2
+    pw = x
     c = g/2 + pw/2
     
     layers = ["F.Cu","F.Paste","F.Mask"]
@@ -77,13 +80,26 @@ for inductor in inductors:
     
     #add inductor courtyard
     cx = c + pw/2
-    cy = y / 2
+    cy = w / 2
     
-    fp.append(RectLine(start=[-cx,-cy],end=[cx,cy],offset=0.35,width=0.05,grid=0.05,layer="F.CrtYd"))
+    fp.append(RectLine(start=[-cx,-cy],end=[cx,cy],offset=0.3,width=0.05,grid=0.05,layer="F.CrtYd"))
     
-    #add lines
-    fp.append(Line(start=[-g/2+0.2,-w/2-0.1],end=[g/2-0.2,-w/2-0.1]))
-    fp.append(Line(start=[-g/2+0.2,w/2+0.1],end=[g/2-0.2,w/2+0.1]))
+    offset = 0.1
+    ly = y/2 + 4 * offset
+    
+    x1 = l / 2 + offset
+    y1 = w / 2 + offset
+    
+    top = [
+    {'x': -x1, 'y': ly},
+    {'x': -x1, 'y': y1},
+    {'x':  x1, 'y': y1},
+    {'x':  x1, 'y': ly},
+    ]
+    
+    
+    fp.append(PolygoneLine(polygone=top))
+    fp.append(PolygoneLine(polygone=top,y_mirror=0))
     
     #Add a model
     fp.append(Model(filename="Inductors.3dshapes/" + fp_name + ".wrl"))
